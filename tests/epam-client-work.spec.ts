56b1bae53ed5eb1c884fd21d689a9355ef0b7fef
Sadique <109426015+MrSadiq85@users.noreply.github.com>
2026-09-03 23:58:34 +0530

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM client work navigation', () => {
+  test('should open Client Work from Services menu', async ({ page }) => {
+    // TODO: implementation
+  });
+});
